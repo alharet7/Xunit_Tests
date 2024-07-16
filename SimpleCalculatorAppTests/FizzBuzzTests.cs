1fe1abb94402383a2a4838b16d0df0c70bb5d0c2
alharet7 <hdhareth@gmail.com>
2024-07-16 16:24:20 +0300

    parametrized Test method Theory And InLineData

---
+++ SimpleCalculatorAppTests/FizzBuzzTests.cs
@@ -0,0 +1,30 @@
+﻿using SimpleCalculatorApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorAppTests
+{
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(4, "4")]
+        public void FizzBuzzTest(int number, string expected)
+        {
+            //Arrange
+            var fizzBuzz = new FizzBuzz();
+
+            //Act
+            var result = fizzBuzz.Generate(number);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
